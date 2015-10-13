1194e7dbe6083e6dfddd09ffce37ba43a82fe4f9
Sravan Suryadevara <sravan.suryadevara@gmail.com>
2015-10-13 10:06:22 -0500

    add player, enemy changes

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    var initialY = [83*0.75,83*1.75,83*2.75];
+    this.x = 0;
+    this.y = initialY[Math.floor(Math.random() * initialY.length)];
 };
 
 // Update the enemy's position, required method for game
@@ -24,12 +27,33 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
 
+    // The image/sprite for our player, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+    this.x = 101*2;
+    this.y = 83*4.75;
+};
+
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.handleInput = function(allowedKeys) {
+
+}
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy(), new Enemy()];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
